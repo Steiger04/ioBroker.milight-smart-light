4410a2f89d9c29e64d1c34ff282e325a647664e2
Steiger04 <mygithub@t-online.de>
2019-01-19 07:29:40 +0100

    ### 0.2.0 (2019-01-16)

---
+++ mslfe-ibr/.nuxt/App.js
@@ -0,0 +1,98 @@
+import Vue from 'vue'
+import NuxtLoading from './components/nuxt-loading.vue'
+
+import '..\\assets\\style\\app.styl'
+
+import _6f6c098b from '..\\layouts\\default.vue'
+
+const layouts = { "_default": _6f6c098b }
+
+export default {
+  head: {"title":"iobroker - milight","meta":[{"charset":"utf-8"},{"name":"viewport","content":"width=device-width, initial-scale=1"},{"hid":"description","name":"description","content":"A small WFE for adapter iobroker.milight-smart-light"},{"hid":"mobile-web-app-capable","name":"mobile-web-app-capable","content":"yes"},{"hid":"apple-mobile-web-app-title","name":"apple-mobile-web-app-title","content":"milight-smart-light FE for iobroker"},{"hid":"author","name":"author","content":"Steiger04"},{"hid":"theme-color","name":"theme-color","content":"#3f51b5"},{"hid":"og:type","name":"og:type","property":"og:type","content":"website"},{"hid":"og:title","name":"og:title","property":"og:title","content":"milight-smart-light FE for iobroker"},{"hid":"og:site_name","name":"og:site_name","property":"og:site_name","content":"milight-smart-light FE for iobroker"},{"hid":"og:description","name":"og:description","property":"og:description","content":"A FE for iobroker.milight-smart-light Adapter."}],"link":[{"rel":"icon","type":"image\u002Fx-icon","href":"~\u002Fassets\u002Fmsl-start-web.png"},{"rel":"stylesheet","href":"https:\u002F\u002Ffonts.googleapis.com\u002Fcss?family=Roboto:300,400,500,700|Material+Icons"},{"rel":"manifest","href":"\u002F_nuxt\u002Fmanifest.585c6db7.json"},{"rel":"shortcut icon","href":"\u002F_nuxt\u002Ficons\u002Ficon_64.e00ww0N08g0.png"},{"rel":"apple-touch-icon","href":"\u002F_nuxt\u002Ficons\u002Ficon_512.e00ww0N08g0.png","sizes":"512x512"}],"style":[],"script":[],"htmlAttrs":{"lang":"de"}},
+
+  render(h, props) {
+    const loadingEl = h('nuxt-loading', { ref: 'loading' })
+    const layoutEl = h(this.layout || 'nuxt')
+    const templateEl = h('div', {
+      domProps: {
+        id: '__layout'
+      },
+      key: this.layoutName
+    }, [ layoutEl ])
+
+    const transitionEl = h('transition', {
+      props: {
+        name: 'layout',
+        mode: 'out-in'
+      },
+      on: {
+        beforeEnter(el) {
+          // Ensure to trigger scroll event after calling scrollBehavior
+          window.$nuxt.$nextTick(() => {
+            window.$nuxt.$emit('triggerScroll')
+          })
+        }
+      }
+    }, [ templateEl ])
+
+    return h('div', {
+      domProps: {
+        id: '__nuxt'
+      }
+    }, [
+      loadingEl,
+      transitionEl
+    ])
+  },
+  data: () => ({
+    layout: null,
+    layoutName: ''
+  }),
+  beforeCreate() {
+    Vue.util.defineReactive(this, 'nuxt', this.$options.nuxt)
+  },
+  created() {
+    // Add this.$nuxt in child instances
+    Vue.prototype.$nuxt = this
+    // add to window so we can listen when ready
+    if (typeof window !== 'undefined') {
+      window.$nuxt = this
+    }
+    // Add $nuxt.error()
+    this.error = this.nuxt.error
+  },
+
+  mounted() {
+    this.$loading = this.$refs.loading
+  },
+  watch: {
+    'nuxt.err': 'errorChanged'
+  },
+
+  methods: {
+    errorChanged() {
+      if (this.nuxt.err && this.$loading) {
+        if (this.$loading.fail) this.$loading.fail()
+        if (this.$loading.finish) this.$loading.finish()
+      }
+    },
+
+    setLayout(layout) {
+      if (!layout || !layouts['_' + layout]) {
+        layout = 'default'
+      }
+      this.layoutName = layout
+      this.layout = layouts['_' + layout]
+      return this.layout
+    },
+    loadLayout(layout) {
+      if (!layout || !layouts['_' + layout]) {
+        layout = 'default'
+      }
+      return Promise.resolve(layouts['_' + layout])
+    }
+  },
+  components: {
+    NuxtLoading
+  }
+}
