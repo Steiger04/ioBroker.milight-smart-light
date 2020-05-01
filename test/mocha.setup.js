813d1d4be6a97c8250f64b1d291e449c763611b2
Steiger04 <mygithub@t-online.de>
2020-05-01 11:49:04 +0200

    ### 0.3.0 (2020-04-30)

--- test/mocha.setup.js
+++ test/mocha.setup.js
@@ -6,9 +6,9 @@ process.on('unhandledRejection', (e) => {
 // enable the should interface with sinon
 // and load chai-as-promised and sinon-chai by default
 const sinonChai = require('sinon-chai');
-const chaiAsPromised = require('chai-as-promised');
+// const chaiAsPromised = require('chai-as-promised');
 const { should, use } = require('chai');
 
 should();
 use(sinonChai);
-use(chaiAsPromised);
+// use(chaiAsPromised);
